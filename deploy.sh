e2231ffc2572ddb88b4edeed6094beb78ed60733
Ubuntu <ubuntu@ip-172-31-21-227.ec2.internal>
2018-12-13 03:42:23 +0000

    a

--- deploy.sh
+++ deploy.sh
@@ -17,11 +17,13 @@ echo "Moving checked out version to versions directory"
 
 cp -r $WORKSPACE $VERSIONS_DIR/$BUILD_TAG
 
-echo "Stop Docker"
-cd $PROJECT_DIR && docker-compose stop
+if [ -d "$PROJECT_DIR" ]; then
+	echo "Stop Docker"
+	cd $PROJECT_DIR && docker-compose stop
 
-echo "Unlink old version"
-cd $HOME_DIR && unlink $PROJECT_NAME
+	echo "Unlink old version"
+	cd $HOME_DIR && unlink $PROJECT_NAME
+fi
 
 echo "Link current version"
 ln -s $VERSIONS_DIR/$BUILD_TAG $PROJECT_DIR
@@ -30,4 +32,4 @@ echo "Set environment parameters for db connection"
 cp $PROJECT_DIR/config/parameters.$ENV.php $PROJECT_DIR/prestashop/app/config/parameters.php
 
 echo "Start Docker"
-cd $PROJECT_DIR && docker-compose start
+cd $PROJECT_DIR && docker-compose up
